a86dde3a8552b463dff9ca02fc28ca791c8d2236
byDavid360 <neurocrazy13@hotmail.com>
2019-10-31 13:05:43 +0100

    Update ServidorFINAL.java

--- ServidorFINAL.java
+++ ServidorFINAL.java
@@ -4,42 +4,30 @@ import java.util.*;
 
 public class ServidorFINAL {
 
-	static int puerto = 25552;
+	static int puerto = 25552;				//declaramos el puerto de la conexion
+	
 	public static void main(String args[]) {
 		
 		try {
 			
-			ServerSocket Conexion = new ServerSocket(puerto);
+			ServerSocket Conexion = new ServerSocket(puerto);		//creamos el socket del servidor en el puerto deseado
 			System.out.println("Servidor creado en el puerto "+puerto);
 			
-	
-			ServerThread T1 = new ServerThread(Conexion, args[0], 1);
-			//T1.start();
+			//Creamos tres hebras que se ocuparán de atender a 3 clientes de forma simultánea pasando como parámetros el socket, el nombre del host (estrá en args[0]) y el número con el que identificaremos a la hebra
+			ServerThread T1 = new ServerThread(Conexion, args[0], 1);	
 			ServerThread T2 = new ServerThread(Conexion, args[0], 2);
 			ServerThread T3 = new ServerThread(Conexion, args[0], 3);
-
-			//Socket socket = Conexion.accept(); //acepta la solicitud que entra al socket
-			//PrintWriter Mostrar = new PrintWriter(socket.getOutputStream(), true);
-			//String Mensaje = "Te has conectado con exito";
-			//Mostrar.println(Mensaje);
-			//LineNumberReader  LLegaDeCliente = new LineNumberReader(new InputStreamReader(socket.getInputStream()));			//con esto cogemos lo que nos llega del cliente
-			
-			//System.out.println(Mensaje + " enviado a: "+ socket.getRemoteSocketAddress());
-
-			
-			
 			
 		} catch (IOException e) {
 			System.out.println("ERROR EN EL SERVER RELACIONADO CON EL SOCKET: "+e);
-		}
-		
+		}	
 	}
 }
 
 
-
-
+/*La clase hebra que atenderá los clientes*/
 class ServerThread extends Thread {
+	
 	ServerSocket MiServidor;
 	Socket Conexion;
 	String NombreMiServer;
@@ -51,82 +39,55 @@ class ServerThread extends Thread {
 		NombreMiServer = NombreServer;
 		MiNumero = NumeroHebra;
 		this.start();
-		
 	}
 	
 	public void run() {
 		
-		System.out.println("Soy la hebra T" + MiNumero + " y estoy en el server "+NombreMiServer);
-		
-		
-		while(true) {		//con el while, hacemos que la hebra cuando quede libre por haberse desconectado el cliente, quede libre para otro nuevo que llegue
-		try {
-			Conexion = MiServidor.accept();
-			PrintWriter Mostrar = new PrintWriter(Conexion.getOutputStream(), true);
-			//String Mensaje = "Te has conectado con exito.";
-			//Mostrar.println(Mensaje);
-			//System.out.println("Enviado a: "+Conexion.getRemoteSocketAddress());
-			
-			System.out.println("Connecction accepted from:  " + Conexion.getInetAddress()+ " at port "+ Conexion.getLocalPort());
-			LineNumberReader LlegaDeCliente = new LineNumberReader(new InputStreamReader(Conexion.getInputStream()));
-		
-			/*String MiLinea = Lector.readLine();
-					
-			while(MiLinea!=null){
-				if(MiLinea.equals(".")) {
-
-					System.out.println("Cerrando conexion...");
-					Conexion.close();
-				}
-				
-				System.out.println("El cliente ha escrito: "+ MiLinea);
-				MiLinea = Lector.readLine();
-			}*/
-			Mostrar.println("Escribe algo: ");
-			String Milinea = LlegaDeCliente.readLine().toString();
-
-
-			while(Milinea!= null ) {
-				//System.out.println(Milinea);		para ver en consola de server lo q escribe el user
-				
-				
-				if(Milinea.equals(".")) {
-					System.out.println("Cerrando conexion...");
-
-					Conexion.close();
-					LlegaDeCliente.close();
-					break;
-				}
-				
-				Date fecha = new Date();
-				String MensajeParaCliente = fecha +" "+ NombreMiServer + " (Hebra T"+ MiNumero +"): " + Milinea;
-				
-		        Mostrar.println(MensajeParaCliente);
-		        Mostrar.println("Escribe algo: ");
-		        
-		        try {
-					Milinea = LlegaDeCliente.readLine().toString();
-
-				} catch (Exception e) {
-		
-				}
-				
-			}
-			
-			
-		
-			//Conexion.close();
-			//LlegaDeCliente.close();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		
-	}
+		while(true) {											//Con este while buscamos que la hebra atienda a un cliente siempre que ésta esté vacía-
+			try {
+				Conexion = MiServidor.accept();							// Aceptamos la conexión con el servidor
+				PrintWriter Mostrar = new PrintWriter(Conexion.getOutputStream(), true);	//
 	
-}
-}
+				System.out.println("Connecction accepted from:  " + Conexion.getInetAddress()+ " at port "+ Conexion.getLocalPort());	//mostramos por consola la IP y el puerto de la conexion actual
+				LineNumberReader LlegaDeCliente = new LineNumberReader(new InputStreamReader(Conexion.getInputStream()));		//Datos que llegan del cliente
+		
+		
+				Mostrar.println("Escribe algo: ");						//Esto se mostrará en la consola del cliente ya que se pasa a través del PrintWriter de salida
+				String Milinea = LlegaDeCliente.readLine().toString();
+
+
+				//siempre que se haya escrito algo desde el cliente
+				while(Milinea!= null ) {			
+				
+					if(Milinea.equals(".")) {	
+						//cerramos conexion si el cliente escribe un 
+						System.out.println("Cerrando conexion...");
+						Conexion.close();
+						LlegaDeCliente.close();
+						break;
+					}
+				
+					Date fecha = new Date();					
+					String MensajeParaCliente = fecha +" "+ NombreMiServer + " (Hebra T"+ MiNumero +"): " + Milinea;
+				
+			        	Mostrar.println(MensajeParaCliente);						//enviamos a través de Mostrar el mensaje que aparecerá en la consola del cliente
+		        		Mostrar.println("Escribe algo: ");
+		        
+		        		try {
+						Milinea = LlegaDeCliente.readLine().toString();
+	
+					} catch (Exception e) {
+		
+					}	
+				}
+	
+			} catch (IOException e) {
+				e.printStackTrace();
+		
+			}
+	
+		}
+	}
 }
 		
 		
