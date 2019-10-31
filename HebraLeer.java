8c4cf73394a408d1247bad69add31e1f90c2973e
byDavid360 <neurocrazy13@hotmail.com>
2019-10-31 12:55:30 +0100

    Update HebraLeer.java

--- HebraLeer.java
+++ HebraLeer.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+/* Esta hebra se ocupará de leer las respuestas eco del servidor y escribirlas en la consola del cliente*/ 
+
 public class HebraLeer extends Thread {
 	
 	Socket MiSocket;
@@ -10,44 +12,30 @@ public class HebraLeer extends Thread {
 	String Linea;
 	
 public HebraLeer(Socket socket) {
+	
 	MiSocket = socket;
-	this.start();
-		
+	this.start();	
 	}
 
 	public void run() {
 		
 		try {
-			LlegaDeServer = new BufferedReader(new InputStreamReader(MiSocket.getInputStream()));
+			LlegaDeServer = new BufferedReader(new InputStreamReader(MiSocket.getInputStream()));	//El BufferedReader recogerá lo que llega del servidor hacia el cliente
 			
 			
 			while(true) {
-				Linea = LlegaDeServer.readLine();
+				Linea = LlegaDeServer.readLine();						//leemos el contenido del BufferedReader
 				
-				if(Linea == null)
+				if(Linea == null)								//cuando no haya contenido, acabaremos la conexion
 					System.exit(0);
 	
-				
-				System.out.println(Linea);
+				System.out.println(Linea);							//escribimos por consola el contenido del BufferedReader
 
 			}
-			
-			
-			
-			
-			
-			
-			
+				
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-		
-
-		
-		
 	}
-	
-	
-	
 }
